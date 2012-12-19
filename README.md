1d12edbcfaab31cdc9dbf945b8174cfac695b0d7
Joel Herron <herronj@gmail.com>
2012-12-19 10:37:32 -0600

    updated docs to include manual theme installation. updated plugin to consolidate actions

--- README.md
+++ README.md
@@ -3,18 +3,37 @@ wp-magazine-control
 
 a version control system for wordpress magazines
 
-
 Usage
 -----
 
 Before activating the plugin add mic_issue and the version number as a custom field on each post. 
 
-On activation the plugin will set the homepage to use only posts from the version set in the admin section. Some custom themes will break this All WPZOOM themes are known to do this.
+On activation the plugin will set the homepage to use only posts from the version set in the admin section. This theme uses the `pre_get_posts()` function to alter The Loop query. Some custom themes will break this. If this plugin does not work out of the box you'll need to add this manually before the `query_posts($args)` call(s) depending on how complex your theme is.
 
-Allows for a beta switch to test the next version 
+    $current_issue = get_option('mic_current_issue');
+    if (defined('MIC_ISSUE')) {
+      $metaq = array(
+        array(
+          'key' => 'mic_issue',
+          'value' => MIC_ISSUE,
+          'compare' => '='
+        )
+      );
+    } else {
+      $metaq = array(
+        array(
+          'key' => 'mic_issue',
+          'value' => $current_issue,
+          'compare' => '='
+        )
+      );
+    }
+    $args['meta_query'] = $metaq;
+
+
+Allows for a beta switch to test the next version this requires the Edit Flow plugin to add a custom post status until this [bug is fixed](http://core.trac.wordpress.org/ticket/12706)
 
 TODO:
 -----
 
-Auto add Beta post status.
-if a version is selected manually find way to keep it going until it's removed 
+allow custom set versions to persist past the main page.
--- magazine-issue-control.php
+++ magazine-issue-control.php
@@ -3,7 +3,7 @@
 Plugin Name: Magazine Issue Control
 Plugin URI: http://h3r2on.com/plugins
 Description: Gives a magazine style blog control over the home page. Allow admin to only show posts that are in one issue while allowing for overrides
-Version: 1.0
+Version: 1.2
 Author: Joel Herron
 Author URI: http://h3r2on.com
 License: CCSA3.0
@@ -17,13 +17,13 @@ register_activation_hook(__FILE__,'magazine_issue_install');
 register_deactivation_hook( __FILE__, 'magazine_issue_remove' );
 
 function magazine_issue_install() {
-    /* Creates new database fields */
-    add_option("mic_current_issue", '1', '', 'yes');
+	/* Creates new database field */
+	add_option("mic_current_issue", '1', '', 'yes');
 }
 
 function magazine_issue_remove() {
-    /* Deletes the database fields */
-    delete_option('mic_current_issue');
+	/* Deletes the database field */
+	delete_option('mic_current_issue');
 }
 
 /* 
@@ -33,109 +33,84 @@ if none defer to pre_get_post action
 */
 
 function magazine_issue_check_override(){
-    if (isset($_GET['beta'])) {
-        $beta_mode = $_GET['beta'];
-        if($beta_mode == 'active') {
-            define("MIC_BETA", $beta_mode);
-        }
-    }
-    // nedd to setup a way to setup a better way to list valid versions and not setup the constant unless it's good
-    $good_versions = array(1,2,3);
-    if (isset($_GET['issue']) && is_numeric($_GET['issue'])) {
-        $override = $_GET['issue'];
-        if (in_array($override,$good_versions)) {
-                define("MIC_ISSUE", $override);
-        }               
-    }
+	if (isset($_GET['beta'])) {
+		$beta_mode = $_GET['beta'];
+		if($beta_mode == 'active') {
+			define("MIC_BETA", $beta_mode);
+		}
+	}
+	// need to setup a way to setup a better way to list valid versions and not setup the constant unless it's good
+	if (isset($_GET['issue']) && is_numeric($_GET['issue'])) {
+		$override = $_GET['issue'];
+		define("MIC_ISSUE", $override);               
+	}
 }
 add_action('init', 'magazine_issue_check_override');
 
-
-function magazine_beta_mode($query) {
-    if(defined('MIC_BETA')) {
-        $query->set('post_status', 'beta');
-    }
+function magazine_issue_show_posts( $query ) {
+	$currrent_issue = get_option('mic_current_issue');
+	if($query->is_home()) {
+		if(defined('MIC_BETA')) {
+			$query->set('post_status', 'beta');
+		} else {
+			if (defined('MIC_ISSUE')) {
+				$metaq = array(
+					array(
+						'key' => 'mic_issue',
+						'value' => MIC_ISSUE,
+						'compare' => '='
+					)
+				);
+			} else {
+				$metaq = array(
+					array(
+						'key' => 'mic_issue',
+						'value' => $current_issue,
+						'compare' => '='
+					)
+				);
+			}
+			$query->set('meta_query', $metaq);
+		}
+	}
 }
-add_action('pre_get_posts','magazine_beta_mode');
+add_action('pre_get_posts','magazine_issue_show_posts');
 
-
-function beta_post_status(){
-  register_post_status( 'beta', array(
-      'label' => _x( 'Beta', 'post' ),
-      'public' => false,
-      'exclude_from_search' => false,
-      'show_in_admin_all_list' => true,
-      'show_in_admin_status_list' => true,
-      'label_count'               => _n_noop( 'Beta <span class="count">(%s)</span>', 'Beta <span class="count">(%s)</span>' ),
-  ) );
-}
-add_action( 'init', 'beta_post_status' );
-
-
-if ( is_home() ) {
-    function magazine_issue_show_posts( $query ) {
-        $cur_issue = get_option('mic_current_issue');
-
-        if (defined('MIC_ISSUE')) {
-            $metaq = array(
-                array(
-                    'key' => 'mic_issue',
-                    'value' => MIC_ISSUE,
-                    'compare' => '='
-                )
-            );
-        } else {
-            $metaq = array(
-                array(
-                    'key' => 'mic_issue',
-                    'value' => $cur_issue,
-                    'compare' => '='
-                )
-            );
-        }
-        $query->set('meta_query', $metaq);
-    }
-    add_action('pre_get_posts','magazine_issue_show_posts');
-}
 
 /*
  * Admin Menu section
  */
 
 if ( is_admin() ){
-
-    /* Call the html code */
-    add_action('admin_menu', 'magazine_issue_admin_menu');
-
-    function magazine_issue_admin_menu() {
-        add_options_page('Magazine Issue Control', 'Magazine Issue Control', 'administrator',
-            'magazine-issue', 'magazine_issue_html_page');
-    }
+	/* Call the html code */
+	add_action('admin_menu', 'magazine_issue_admin_menu');
 }
 
-function magazine_issue_html_page() {
-?>
-    <div>
-        <h2>Magazine Issue Control Options</h2>
-
-        <form method="post" action="options.php">
-        <?php wp_nonce_field('update-options'); ?>
-
-        <table width="510">
-            <tr valign="top">
-                <th width="92" scope="row">Set the Current Issue</th>
-                <td width="406">
-                    <input name="mic_current_issue" type="text" id="mic_current_issue" value="<?php echo get_option('mic_current_issue'); ?>" />
-                    (ex. 1)</td>
-            </tr>
-        </table>
-        <input type="hidden" name="action" value="update" />
-        <input type="hidden" name="page_options" value="mic_current_issue" />
-        <p>
-            <input type="submit" value="<?php _e('Save Changes') ?>" />
-        </p>
-        </form>
-    </div>
-<?php
+function magazine_issue_admin_menu() {
+	add_options_page('Magazine Issue Control', 'Magazine Issue Control', 'administrator',
+		'magazine-issue', 'magazine_issue_html_page');
 }
-?>
+
+function magazine_issue_html_page() { ?>
+	<div>
+		<h2>Magazine Issue Control Options</h2>
+
+		<form method="post" action="options.php">
+		<?php wp_nonce_field('update-options'); ?>
+
+		<table width="510">
+			<tr valign="top">
+				<th width="92" scope="row">Set the Current Issue</th>
+				<td width="406">
+					<input name="mic_current_issue" type="text" id="mic_current_issue" value="<?php echo get_option('mic_current_issue'); ?>" />
+					(ex. 1)</td>
+			</tr>
+		</table>
+		<input type="hidden" name="action" value="update" />
+		<input type="hidden" name="page_options" value="mic_current_issue" />
+		<p>
+			<input type="submit" value="<?php _e('Save Changes') ?>" />
+		</p>
+		</form>
+	</div>
+<?php } ?>
